d2b54952bf71f8bca6f7a7f10825139ad27a584f
simon-mwangi5499 <mwangi.simon5499@gmail.com>
2023-09-11 15:52:51 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+c = 0;
+while
+	(c < 10) {
+		putchar(c + '0');
+		c++;
+	}
+putchar('\n');
+return (0);
+}
