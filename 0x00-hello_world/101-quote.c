396fa0ce55c87b902fe5237281625fd371e187e6
simon-mwangi5499 <mwangi.simon5499@gmail.com>
2023-09-11 09:42:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line standard error.
+ *
+ * Return 0(on success, non-zero on failures)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (0);
+}
