dc286ebb83d5bc6a047c4a19b3618eaa40a68bc5
simon-mwangi5499 <mwangi.simon5499@gmail.com>
2023-09-11 14:38:41 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+char d;
+c = 'a';
+d = 'A';
+while
+
+
+	(c <= 'z') {
+		putchar(c);
+		c++;
+	}
+while
+	(d <= 'z') {
+		putchar(d);
+		d++;
+	}
+putchar('\n');
+return (0);
+}
