8286c2f369ade5fda848f31305c56c4da0f10e8f
simon-mwangi5499 <mwangi.simon5499@gmail.com>
2023-09-25 14:55:06 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch - 'a'; ch <= 'a'; ch++)
+	_putchar(ch);
+	putchar('\n');
+}
